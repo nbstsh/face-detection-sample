303863dee4f2fc66b413b9d84fa10ca46406fa4f
nbstsh <nbstsh@gmail.com>
2019-06-28 21:56:56 +0900

    Basic face detection

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const videoBoxEl = document.querySelector('.video-box');
+const videoEl = document.querySelector('#video');
+
+const startVideo = async () => {
+	try {
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: true,
+			audio: false
+		});
+		videoEl.srcObject = stream;
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
+
+videoEl.addEventListener('play', () => {
+	const canvas = faceapi.createCanvasFromMedia(video);
+	videoBoxEl.appendChild(canvas);
+	const displaySize = { width: video.width, height: video.height };
+	faceapi.matchDimensions(canvas, displaySize);
+	setInterval(async () => {
+		const detections = await faceapi
+			.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+			.withFaceLandmarks()
+			.withFaceExpressions();
+		const resizedDetections = faceapi.resizeResults(
+			detections,
+			displaySize
+		);
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		faceapi.draw.drawDetections(canvas, resizedDetections);
+		faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+		faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+	}, 100);
+});
